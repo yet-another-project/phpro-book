a1a4e97a4e63af6b45b8be5b4dc15ceafce6d0f8
Flavius Aspra <flavius.as@gmail.com>
2012-02-17 16:05:06 +0100

    comunicarea php - mysqli

--- cap04/1-mysqli-connect.php
+++ cap04/1-mysqli-connect.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'minerva2');
+$connection = mysqli_connect('localhost', 'root', 'root');
 if($connection) {
     $result = mysqli_query($connection, 'SHOW DATABASES');
     $databases = mysqli_fetch_all($result, MYSQLI_ASSOC);
